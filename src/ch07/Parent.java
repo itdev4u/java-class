e0f84412e3704d46473c5b7aea2aae7ab1dc6255
itdev4u <luna7698@gmail.com>
2025-01-26 01:19:20 +0900

    0126-2

---
+++ src/ch07/Parent.java
@@ -0,0 +1,5 @@
+package ch07;
+
+public class Parent {
+    int x = 10;
+}
---
+++ src/ch07/SuperTest2.java
@@ -0,0 +1,8 @@
+package ch07;
+
+public class SuperTest2 {
+    public static void main(String[] args) {
+        Child2 c = new Child2();
+        c.method();
+    }
+}
