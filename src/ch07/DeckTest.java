4ad6b31e6aeffcb6401103c88f45410e8fd93439
itdev4u <luna7698@gmail.com>
2025-01-26 00:16:02 +0900

    0126

---
+++ src/ch07/DeckTest.java
@@ -0,0 +1,13 @@
+package ch07;
+
+public class DeckTest {
+    public static void main(String[] args) {
+        Deck d = new Deck();
+        Card c = d.pick(0);
+        System.out.println(c);
+
+        d.shuffle();
+        c = d.pick(0);
+        System.out.println(c);
+    }
+}
