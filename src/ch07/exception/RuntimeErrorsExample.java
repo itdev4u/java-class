fc06015389cbefa02da17b10868a835c37c237a3
lunajin <luna7698@gmail.com>
2025-02-07 10:36:36 +0900

    2007_2

--- src/ch07/exception/RuntimeErrorsExample.java
+++ src/ch07/exception/RuntimeErrorsExample.java
@@ -8,7 +8,11 @@ public class RuntimeErrorsExample {
         /* STEP02 : ArithmeticException */
         //System.out.println(12/0);
         /* STEP03 : NullPointException */
-        String s = null;
-        System.out.println(s.length());
+        //String s = null;
+        //System.out.println(s.length());
+        /* STEP04 : NumberFormatException ==> Float.parseFloat */
+        //String stringNumber = "3.141592";
+        //int num = Integer.parseInt(stringNumber);
+        //float num = Float.parseFloat(stringNumber);
     }
 }
