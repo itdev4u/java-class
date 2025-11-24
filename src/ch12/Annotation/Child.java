caf3812539ffb8ef36fb329b36e84f5c2ea5ecef
REDDAL <reddal7698@gmail.com>
2025-11-24 23:58:53 +0900

    삭제

--- src/ch12/Annotation/Child.java
+++
@@ -1,7 +0,0 @@
-package ch12.Annotation;
-
-public class Child extends Parent {
-    @Override
-    //void parentmethod() { }
-    void parentMethod() { }
-}
--- src/ch12/Annotation/Parent.java
+++
@@ -1,7 +0,0 @@
-package ch12.Annotation;
-
-public class Parent {
-    void parentMethod() {
-
-    }
-}
